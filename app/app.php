6a73eb4a8f31b2775070f178ac0239bdd3bdadbc
nathan young & Ben Spenard <student@epicodus.com>
2015-08-05 12:07:44 -0700

    stated adding delete button

--- app/app.php
+++ app/app.php
@@ -13,11 +13,21 @@
 
         $output = "";
 
-        foreach(Task::getAll() as $task) {
-            $output = $output . "<p>" . $task->getDescription() . "</p>;"
+        $all_Tasks = Task::getAll();
+
+        if (!empty($all_tasks)) {
+            $output = $output . "
+            <h1>To Do List</h1>
+            <p>Here are all your tasks:</p>
+            ";
+
+            foreach ($all_tasks as $task) {
+                $output = $output . "<p>" . $task->getDescription() . "</p>";
+            }
+
         }
 
-        $output = $output . "
+        $output .= "
             <form action='/tasks' method='post'>
                 <label for='description'>Task Description</label>
                 <input id='description' name='description' type='text'>
@@ -26,7 +36,31 @@
             </form>
         ";
 
+        $output .= "
+            <form action='/delete_tasks' method='post'>
+                <button type='submit'>delete</button>
+            </form>
+        ";
+
         return $output;
+    });
+
+    $app->post("/tasks", function() {
+        $task = new Task($_POST['description']);
+        $task->save();
+        return "
+            <h1>You created a task!</h1>
+            <p>" . $task->getDescription() . "</p>
+            <p><a href='/'>View your list of things to do.</a></p>
+        ";
+    });
+
+    $app->post("/delete_tasks", function() {
+        Task::deleteAll();
+        return "
+            <h1>List Cleared!</h1>
+            <p><a href='/'>Home</a></p>
+        ";
     });
 
     return $app;
