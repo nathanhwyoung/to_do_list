83e4def27f8422017d9bccca4582c4c6b3338f69
nathan young & austin blanchard <nhwilcox@gmail.com>
2015-08-17 15:59:31 -0700

    adds tests and DB functionality

--- src/Task.php
+++ src/Task.php
@@ -2,10 +2,17 @@
 class Task
 {
     private $description;
+    private $id;
 
-    function __construct($description)
+    function __construct($description, $id = null)
     {
         $this->description = $description;
+        $this->id = $id;
+    }
+
+    function getId()
+    {
+        return $this->id;
     }
 
     function setDescription($new_description)
@@ -20,16 +27,18 @@ class Task
 
     function save()
     {
-        $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}')");
+        $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+        $this->id = $GLOBALS['DB']->lastInsertId();
     }
 
     static function getAll()
     {
         $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
         $tasks = array();
-        $foreach($returned_tasks as $task) {
+        foreach($returned_tasks as $task) {
             $description = $task['description'];
-            $new_task = new Task($description);
+            $id = $task['id'];
+            $new_task = new Task($description, $id);
             array_push($tasks, $new_task);
         }
         return $tasks;
@@ -37,7 +46,20 @@ class Task
 
     static function deleteAll()
     {
-        $_SESSION['list_of_tasks'] = array();
+        $GLOBALS['DB']->exec("DELETE FROM tasks;");
+    }
+
+    static function find($search_id)
+    {
+        $found_task = null;
+        $tasks = Task::getAll();
+        foreach($tasks as $task) {
+            $task_id = $task->getId();
+            if ($task_id == $search_id) {
+                $found_task = $task;
+            }
+        }
+        return $found_task;
     }
 }
 ?>
--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -7,13 +7,17 @@
 
     require_once "src/Task.php";
 
-    $server = 'mysql:host;dbname=to_do_test';
+    $server = 'mysql:host=localhost;dbname=to_do_test';
     $username = 'root';
     $password = 'root';
     $DB = new PDO ($server, $username, $password);
 
     class TaskTest extends PHPUnit_Framework_TestCase
     {
+        protected function tearDown()
+        {
+            Task::deleteAll();
+        }
 
         function test_save()
         {
@@ -44,6 +48,39 @@
 
             //assert
             $this->assertEquals([$test_task, $test_task2], $result);
+        }
+
+        function test_getId()
+        {
+
+            //arrange
+            $description = "Wash the dog";
+            $id = 1;
+            $test_Task = new Task($description, $id);
+
+            //act
+            $result = $test_Task->getId();
+
+            //
+            $this->assertEquals(1, $result);
+        }
+
+        function test_find()
+        {
+            //arrange
+            $description = "Wash the dog";
+            $description2 = "Water the lawn";
+            $test_task = new Task($description);
+            $test_task->save();
+            $test_task2 = new Task($description2);
+            $test_task2->save();
+
+            //act
+            $id = $test_task->getId();
+            $result = Task::find($id);
+
+            //assert
+            $this->assertEquals($test_task, $result);
         }
     }
 
